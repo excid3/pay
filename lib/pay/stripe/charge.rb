0b71aced46962136497521684ac64dc4f6f63fbf
Romain Manguin <harlequin.crest@gmail.com>
2019-12-06 18:07:59 +0100

    Handles delayed capture for a PaymentIntent

--- lib/pay/stripe/charge.rb
+++ lib/pay/stripe/charge.rb
@@ -8,8 +8,24 @@ module Pay
         processor == "stripe"
       end
 
+      # Doesn't work in jumpstartpro
+      # #######
+      # Team.first.charges.first.stripe_charge
+      # > NoMethodError: undefined method `retrieve' for Pay::Stripe::Charge:Module
       def stripe_charge
         Stripe::Charge.retrieve(processor_id)
+      rescue ::Stripe::StripeError => e
+        raise Error, e.message
+      end
+
+      # To capture a Charge created through a PaymentIntent we have to capture the PaymentIntent itself.
+      # #######
+      # Stripe::Charge.retrieve(processor_id).capture
+      # > Stripe::InvalidRequestError: This uncaptured Charge was created by a PaymentIntent. You must capture the PaymentIntent instead.
+      def stripe_capture
+        Pay::Payment.from_id(payment_intent).capture
+        update(captured: true)
+
       rescue ::Stripe::StripeError => e
         raise Error, e.message
       end
